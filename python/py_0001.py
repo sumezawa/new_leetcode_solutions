ed257c0fff4e871bb68d1bd014603d2f1c78967c
Sammy Umezawa <Umezawa@institut2b.physik.rwth-aachen.de>
2025-11-19 11:14:57 +0100

    Comments to Leetcode 0001

--- python/py_0001.py
+++ python/py_0001.py
@@ -7,11 +7,11 @@ class solver:
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
-        
-        Given an array of integers nums and an integer target, 
+
+        Given an array of integers nums and an integer target,
         return indices of the two numbers such that they add up to target.
 
-        You may assume that each input would have exactly one solution, and 
+        You may assume that each input would have exactly one solution, and
         you may not use the same element twice.
 
         You can return the answer in any order.
@@ -27,7 +27,6 @@ class solver:
             if target - nums[i] in dict_of_nums:
                 if dict_of_nums[target - nums[i]] != i:
                     return [i,dict_of_nums[target - nums[i]]]
-            
+            # first if statement queries existence of desired summand
+            # second if statement makes sure same element isn't used twice
         return [0,0]
-
-    
